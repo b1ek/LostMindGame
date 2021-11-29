a12dabf5979faea552ed2701e1aefdfc34337b17
b1ek <creeperywime@gmail.com>
2021-11-30 00:55:31 +1000

    RegistryConfig

--- Engine/Config/RegistryConfig.cs
+++ Engine/Config/RegistryConfig.cs
@@ -5,47 +5,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LostMind.Engine.Config
-{
-    static class RegistryConfig
-    {
-        public static RegistryKey root = Registry.CurrentUser.CreateSubKey("SOFTWARE").CreateSubKey("blek").CreateSubKey("LostMind");
-        //                               HKEY_CURRENT_USER/SOFTWARE/blek/LostMind
+namespace LostMind.Engine.Config {
+    class RegistryConfig {
+        RegistryKey baseKey = Registry.CurrentUser;
+        public RegistryKey BaseKey {
+            get => baseKey;
+            set {
+                baseKey = value.CreateSubKey(rootPath);
+            }
+        }
+        string path = "";
+        public string Path {
+            get => path;
+            set {
+                path = value;
+            }
+        }
+        string _rootPath;
+        public RegistryConfig(string rootPath) {
+            _rootPath = rootPath;
+            baseKey = baseKey.CreateSubKey(rootPath);
+        }
+        public object? GetValue(string key, object? defaultValue = null) {
+            return baseKey.GetValue(key, defaultValue);
+        }
+        public string  GetValueString(string key, string def = "{EMPTY}") {
+            var val = GetValue(key, null);
+            if (val == null) val = def;
+            return (string)val;
+        }
+        public int     GetValueInt(string key, int def = 0) {
+            var val = GetValue(key, null);
+            if (val == null | int.TryParse((string)val, out _)) val = def;
 
-        public static bool startGameWithoutBootloader {
-            get {
-                var val = root.GetValue("startGameWithoutBootload", false);
-                if (val == null) return false;
-                return val.ToString().ToLower() == "true";
-            }
+            return int.Parse((string) val);
         }
-        public static bool noStartupLogoAnim {
-            get {
-                var val = root.GetValue("noStartupLogoAnim", false);
-                if (val == null) return false;
-                return val.ToString().ToLower() == "true";
-            }
+        public bool    GetValueBool(string key, bool def = false) {
+            var val = GetValue(key, null);
+            if (val == null) val = def.ToString();
+            return ((string) val).ToLower() == "true";
         }
-        public static bool AllowBSODStyleException {
-            get {
-                var val = root.GetValue("AllowBSODStyleException", false);
-                if (val == null) return false;
-                return val.ToString().ToLower() == "true";
-            }
+
+        public void    SetValue   (string key, string value, RegistryValueKind kind = RegistryValueKind.String) {
+            baseKey.SetValue(key, value, kind);
         }
-        public static string customGameTitle {
-            get {
-                var val = root.GetValue("customGameTitle", null);
-                if (val == null) return null;
-                return Convert.ToString(val);
-            }
-        }
-        public static bool localeSafe {
-            get {
-                var val = root.GetValue("localeSafe", null);
-                if (val == null) return true;
-                return val.ToString().ToLower() == "true";
-            }
+        public bool    TrySetValue(string key, string value, RegistryValueKind kind = RegistryValueKind.String) {
+            try {
+                baseKey.SetValue(key, value, kind); return true;
+            } catch (Exception) { return false; }
         }
     }
 }
